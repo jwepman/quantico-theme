1787b13c01ef2f2cba042ff00e93d09580eebbf9
Josh Wepman <joshua.wepman@colorado.edu>
2011-12-24 03:06:29 -0700

    Cleaned up tags and date display and added system-message class for system-generated messages. Still need to fix tag search, categories, pages, menu, etc.

--- loop.php
+++ loop.php
@@ -4,7 +4,8 @@
 		<div class="content-body">
 			<?php the_content(); ?>
 		</div>
+		<span class="content-tags"> <?php the_tags( 'Tags: ', " | ", '' ); ?> </span><span class="content-date">Posted: <?php the_time('F j, Y'); ?> at <?php the_time('g:i a'); ?>.</span>
 	</div>
 	<?php endwhile; else: ?>
-<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+<div class="content-container system-message"><?php _e('Sorry, no posts matched your criteria.'); ?></div>
 <?php endif; ?>
